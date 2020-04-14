707d0b43e91f02b611c312f9096200a099fef251
AlexsandrVanin <vaninalexsandr98gmail.com>
2020-04-15 01:30:07 +0300

    Updates

---
+++ precache-manifest.e5389dd2a3fe1547d1813c030c46c3ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5399574be8f22b6ada8933df7448eb4",
+    "url": "/Movie-react/index.html"
+  },
+  {
+    "revision": "ac38e17da378b41d93d5",
+    "url": "/Movie-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ef939e1aa04cb45865a1",
+    "url": "/Movie-react/static/css/main.cbc561df.chunk.css"
+  },
+  {
+    "revision": "ac38e17da378b41d93d5",
+    "url": "/Movie-react/static/js/2.beffbc43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-react/static/js/2.beffbc43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef939e1aa04cb45865a1",
+    "url": "/Movie-react/static/js/main.337c4a40.chunk.js"
+  },
+  {
+    "revision": "51dcc73069fa09f0f34d",
+    "url": "/Movie-react/static/js/runtime-main.69f20b11.js"
+  }
+]);
